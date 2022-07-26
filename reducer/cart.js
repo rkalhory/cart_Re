acceba3cdd4a9cdebd1f0633cd0e9f87d355316e
roya kalhory <rkalhory@gmail.com>
2022-07-26 15:14:59 +0430

    v1

---
+++ reducer/cart.js
@@ -0,0 +1,28 @@
+const cart=[];
+
+const cart=(state=cart,action)=>{
+    const product=action.payload;
+    switch (action.type) {
+        case 'ADDITEM':
+            const exist=state.find((x)=>x.id===product.id)
+            if(exist){
+                return state.map(x=>
+                x.id===product.id?{...x,qty:x.qty+1}:x)
+            }else{
+                return [...state, {product,qty:1}]
+            }
+            break;
+        case 'DELITEM':
+            const exist1=state.find((x)=>x.id===product.id)
+            if(exist1.qty===1){
+                return state.filter(x=>x.id==!exist1.id)
+            }else{
+                return state.map(x=>
+                    x.id===product.id?{...x,qty:x.qty-1}:x
+                )
+            }
+            break;
+    }
+}
+
+export  default cart
---
+++ reducer/index.js
@@ -0,0 +1,8 @@
+import cart from './cart'
+import {combineReducers} from 'redux'
+
+const reducer=combineReducers({
+    cart,
+})
+
+export default reducer
