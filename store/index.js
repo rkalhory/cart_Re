1b39e02c1ebfda562ed698b1d6c98abe63cba5e5
rkalhory <70099091+rkalhory@users.noreply.github.com>
2022-10-09 19:11:52 +0330

    Update index.js

--- store/index.js
+++ store/index.js
@@ -4,3 +4,18 @@ import reducer from "../reducer";
 const store=createStore(reducer)
 
 export default store
+
+
+
+
+
+
+
+
+import {createStore, applyMiddleware} from 'redux'
+import thunk  from 'redux-thunk'
+import reducer from '../reducer'
+
+const store=createStore(reducer,applyMiddleware(thunk))
+
+export default store
